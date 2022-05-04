3e852582433834662d168c442497f470d0b94a76
bodascaluca <bodasca.luca@gmail.com>
2022-05-04 17:45:04 +0200

    Milestone-3

--- js-jsnack-blocco-3/js/script.js
+++ js-jsnack-blocco-3/js/script.js
@@ -1,6 +1,4 @@
 
-// 2. Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando a somma degli elementi è minore di 50.
-// 3. Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0. Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
 // **BONUS
 // Crea due tag div con due id diversi. Un div avrà il testo colorato di rosso mentre l’altro di verde. Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
 
@@ -11,7 +9,23 @@
 
 // const menoNumeri = ["macchina", "casa", "ruota", "sasso",];
 
+// if (numbers.length > menoNumeri.length) {
+//     for (let i = menoNumeri.length; i < numbers.length; i++) {
+//         const num = randomNumbers(1, 100);
+//         menoNumeri.push(num);
+//     }
+//     console.log('vuol dire che dobbiamo aggiungere valori all array MENONUMERI, quanti?');
+// }
 
+// if (menoNumeri.length > numbers.length) {
+//     for (let i = numbers.length; i < menoNumeri.length; i++) {
+//         const num = randomNumbers(1, 100);
+//         numbers.push(num);
+//     }
+//     console.log('vuol dire che dobbiamo aggiungere valori all array MENONUMERI, quanti?');
+// }
+
+///////////////
 // let numbersIndex = 0;
 // numbers.forEach((element, index)=>{
 //     numbersIndex = index;
@@ -35,7 +49,8 @@
 
 // console.log(menoNumeri);
 
-
+////////////
+//////////Si ripete anche per me 
 // function randomNumbers(min, max){
 //     return Math.floor(Math.random() * (max - min + 1) ) + min;
 // }
@@ -43,44 +58,59 @@
 // MILLESTON 2
 // 2. Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando a somma degli elementi è minore di 50.
 
+// const array = [];
+// console.log(array)
+
+// let laSomma = 0;
+
+// do{
+//     const askClient = parseInt(prompt(`Dimmi un numero`));
+//     array.push(askClient);
+//      laSomma += askClient;
+// } while (laSomma <= 50);
+//     console.log(array);
+//     console.log(laSomma)
+
+
+// 3. Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0. Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
+
+const squadre = [
+    {
+        nome:"Toro",
+        puntiFatti:0,
+        falliSubiti:0,
+    },
+
+    {
+        nome:"Fiorentina",
+        puntiFatti:0,
+        falliSubiti:0,
+    },
+
+    {
+        nome:"Sassuol0",
+        puntiFatti:0,
+        falliSubiti:0,
+    },
+];
+console.log(squadre);
+
+const newArray = squadre.map((element)=>{
+
+    const container = {};
+    container.nome = element.nome;
+    container.puntiFatti = random(0,50);
+    container.falliSubiti= random(0,50);
+    return container;
+});
+
+console.log(newArray);
 
 
 
-const array = [10,12,3];
-console.log(array)
-
-let laSomma = 0;
-let i = 0;
-
-do{
-    
-    const askClient = parseInt(prompt(`Dimmi un numero`));
-    array.push(askClient);
-     laSomma = theSum(array)
-    laSomma++;
-    }while (laSomma <= 50 );
-    
-    console.log(laSomma)
-
-
-function theSum(allNumbers){
-    let sum = 0;
-    for (let i = 0; i < array.length; i++){
-        let numbers  = array[i];
-        sum = sum + numbers
-    }
-    return sum;
+function random(min,max ){
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
-
-// array.forEach((element, index)=>{
-//     element[i] = element;
-//     console.log(element);
-//     // numbers += element 
-//     // console.log(numbers)
-// });
-// console.log(numbers);
-
-
 
 
 
